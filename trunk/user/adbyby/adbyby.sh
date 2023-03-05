b0bde64ab64cbc39d630026c090453612a2714d8
panybbib <pan.107@163.com>
2023-03-05 19:53:39 +0800

    Update adbyby.sh

--- trunk/user/adbyby/adbyby.sh
+++ trunk/user/adbyby/adbyby.sh
@@ -78,15 +78,15 @@ add_rules()
 	if [ "$lazy_online"x != "$lazy_local"x -o "$video_online"x != "$video_local"x ]; then
 	echo "MD5 not match! Need update!"
 	logger -t "adbyby" "更新规则,下载中..."
-	#touch /tmp/lazy.txt && curl -k -s -o /tmp/lazy.txt --connect-timeout 5 --retry 3 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
-	#touch /tmp/video.txt && curl -k -s -o /tmp/video.txt --connect-timeout 5 --retry 3 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
+	touch /tmp/lazy.txt && curl -k -s -o /tmp/lazy.txt --connect-timeout 5 --retry 3 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt
+	touch /tmp/video.txt && curl -k -s -o /tmp/video.txt --connect-timeout 5 --retry 3 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt
 	touch /tmp/local-md5.json && md5sum /tmp/lazy.txt /tmp/video.txt > /tmp/local-md5.json
 	lazy_local=$(grep 'lazy' /tmp/local-md5.json | awk -F' ' '{print $1}')
 	video_local=$(grep 'video' /tmp/local-md5.json | awk -F' ' '{print $1}')
 	if [ "$lazy_online"x == "$lazy_local"x -a "$video_online"x == "$video_local"x ]; then
 	echo "New rules MD5 match!"
 	mv /tmp/lazy.txt $PROG_PATH/data/lazy.txt
-	mv /tmp/video.txt $PROG_PATH/data/video.txt
+	#mv /tmp/video.txt $PROG_PATH/data/video.txt
 	echo $(date +"%Y-%m-%d %H:%M:%S") > /tmp/adbyby.updated
 	fi
 	else
