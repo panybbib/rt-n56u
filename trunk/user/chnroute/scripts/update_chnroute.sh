a277b89b8cf4b35b74d8872ca2cc0415a0f5d384
panybbib <pan.107@163.com>
2023-08-13 12:11:08 +0800

    update

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -5,14 +5,14 @@ set -e -o pipefail
 [ "$1" != "force" ] && [ "$(nvram get ss_update_chnroute)" != "1" ] && exit 0
 CHNROUTE_URL="$(nvram get ss_chnroute_url)"
 
-logger -st "chnroute" "Starting update..."
+logger -st "chnroute" "CHNRoute 开始更新..."
 rm -f /tmp/chinadns_chnroute.txt
 
 if [ -z "$CHNROUTE_URL" ]; then
-	curl -k -s --connect-timeout 20 --retry 3 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | \
+	curl -k -s --connect-timeout 5 --retry 3 http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | \
 		awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/chinadns_chnroute.txt
 else
-	curl -k -s --connect-timeout 20 --retry 3 -o /tmp/chinadns_chnroute.txt "$CHNROUTE_URL"
+	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/chinadns_chnroute.txt "$CHNROUTE_URL"
 fi
 
 [ ! -d /etc/storage/chinadns/ ] && mkdir /etc/storage/chinadns/
@@ -22,4 +22,4 @@ mtd_storage.sh save >/dev/null 2>&1
 
 [ -f /usr/bin/shadowsocks.sh ] && [ "$(nvram get ss_enable)" = "1" ] && [ "$(nvram get ss_run_mode)" = "router" ] && /usr/bin/shadowsocks.sh restart >/dev/null 2>&1
 
-logger -st "chnroute" "Update done"
+logger -st "chnroute" "CHNRoute 更新完成..."
