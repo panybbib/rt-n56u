baed27187fa2dd2efc8e1d9cc2143f54cddffc48
panybbib <pan.107@163.com>
2023-08-17 00:24:20 +0800

    update

--- trunk/user/shadowsocks/scripts/shadowsocks.sh
+++ trunk/user/shadowsocks/scripts/shadowsocks.sh
@@ -22,6 +22,7 @@ v2_json_file="/tmp/v2-redir.json"
 trojan_json_file="/tmp/tj-redir.json"
 server_count=0
 redir_tcp=0
+trojan_enable=0
 v2ray_enable=0
 redir_udp=0
 tunnel_enable=0
@@ -131,7 +132,7 @@ gen_config_file() {
 				fi
 			fi
 		fi
-		v2ray_enable=1
+		trojan_enable=1
 		if [ "$2" = "0" ]; then
 			lua /etc_ro/ss/gentrojanconfig.lua $1 nat 1080 >$trojan_json_file
 			sed -i 's/\\//g' $trojan_json_file
@@ -586,10 +587,10 @@ rules() {
 
 start_watchcat() {
 	if [ $(nvram get ss_watchcat) = 1 ]; then
-		let total_count=server_count+redir_tcp+redir_udp+tunnel_enable+v2ray_enable+local_enable+pdnsd_enable_flag+chinadnsng_enable_flag
+		let total_count=server_count+redir_tcp+redir_udp+tunnel_enable+trojan_enable+v2ray_enable+local_enable+pdnsd_enable_flag+chinadnsng_enable_flag
 		if [ $total_count -gt 0 ]; then
 			#param:server(count) redir_tcp(0:no,1:yes)  redir_udp tunnel kcp local gfw
-			/usr/bin/ssr-monitor $server_count $redir_tcp $redir_udp $tunnel_enable $v2ray_enable $local_enable $pdnsd_enable_flag $chinadnsng_enable_flag >/dev/null 2>&1 &
+			/usr/bin/ssr-monitor $server_count $redir_tcp $redir_udp $tunnel_enable $trojan_enable $v2ray_enable $local_enable $pdnsd_enable_flag $chinadnsng_enable_flag >/dev/null 2>&1 &
 		fi
 	fi
 }
